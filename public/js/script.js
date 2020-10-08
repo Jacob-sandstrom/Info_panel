3db49cd15627ba256b1c4dbe244cf311c8d48b08
Jacob Sandström <jojacocobob@gmail.com>
2020-10-08 16:10:49 +0200

    made javascript handle the render functionality

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+async function renderData() {
+    renderBussData()
+}
+
+
+async function renderBussData() {
+
+    const response = await fetch(`http://localhost:9292/api/busses`);
+    const result = await (response.json());
+
+    // console.log(result)
+    bussBox = document.createElement("div")
+    bussBox.classList.add("bussBox")
+    for (const element of result) {
+
+        bussName = document.createElement("div")
+        bussName.classList.add("bussName")
+        bussName.innerHTML = element.name
+
+        bussDepTime = document.createElement("div")
+        bussDepTime.classList.add("bussDepTime")
+        bussDepTime.innerHTML = element.dep_time
+
+
+        buss = document.createElement("article")
+        buss.classList.add("buss")
+
+        buss.appendChild(bussName)
+        buss.appendChild(bussDepTime)
+
+        bussBox.appendChild(buss)
+
+    }
+    document.querySelector(".wrapper").appendChild(bussBox)
+
+}
